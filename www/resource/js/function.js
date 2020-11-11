563669ece6be52405cf2b493093901d1464b6f4e
Taekwang <tkatls1013@naver.com>
2020-11-11 16:19:43 +0900

    메인페이지 작업 1111/16:19

--- www/resource/js/function.js
+++ www/resource/js/function.js
@@ -16,7 +16,45 @@ $(document).ready(function(){
 		},
 	});
 
+	//메인 센터소식
+	var swiper = new Swiper('#main-center-list', {
 
+		slidesPerView: 4,
+		spaceBetween: 17,
+		// slidesPerGroup: 4,
+		loop: true,
+		// loopFillGroupWithBlank: true,
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false,
+		},
+		navigation: {
+			nextEl: '#main-center .swiper-button-box .swiper-button-next',
+			prevEl: '#main-center .swiper-button-box .swiper-button-prev',
+		},
+	});
+
+	//메인 사업안내
+	var swiper = new Swiper('#main-prod-list', {
+
+		slidesPerView: 3,
+		spaceBetween: 140,
+		centeredSlides: true,
+		loop: true,
+		// loopFillGroupWithBlank: true,
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false,
+		},
+		pagination: {
+			el: '#main-prod .swiper-pagination',
+			clickable: true,
+		},
+		navigation: {
+			nextEl: '#main-prod .swiper-button-box .swiper-button-next',
+			prevEl: '#main-prod .swiper-button-box .swiper-button-prev',
+		},
+	});
 
 	//메인메뉴 on/off
 	$("#gnb li a").mouseenter(function(){
