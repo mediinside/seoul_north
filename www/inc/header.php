9b5642a23f0aef380c73033cd0d40b36606488ee
Taekwang <tkatls1013@naver.com>
2020-11-20 17:51:30 +0900

    퍼블리싱 1120/17:51

--- www/inc/header.php
+++ www/inc/header.php
@@ -91,8 +91,8 @@
 								<dd><a href="/center/page02-1.html">사업개요</a></dd>
 								<dd><a href="/center/page03.html">전국센터현황</a></dd>
 								<dd><a href="/center/page04-1.html">업무협력기관</a></dd>
-								<dd><a href="#">직원안내</a></dd>
-								<dd><a href="#">오시는 길</a></dd>
+								<dd><a href="/center/page05.html">직원안내</a></dd>
+								<dd><a href="/center/page06.html">오시는 길</a></dd>
 							</dl>
 							<dl>
 								<dt><a href="#">사업안내</a></dt>
